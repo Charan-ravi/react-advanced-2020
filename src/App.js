84a5fbcc08729e5d3ce5de795fd29b094957d3ca
Charan-ravi <charan2ravi@gmail.com>
2022-03-30 12:03:18 +0530

    saving...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 // import Setup from './tutorial/1-useState/setup/1-error-example'
 // import Setup from './tutorial/1-useState/setup/2-useState-basics'
-import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
+// import Setup from "./tutorial/10-prop-types/setup";
+// import Final from "./tutorial/11-react-router/final";
+import Setup from "./tutorial/11-react-router/setup";
 function App() {
   return (
     <div className="container">
--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,25 @@
-import React from 'react';
-
-const Product = () => {
-  return <article className='product'>single product</article>;
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
+const Product = ({ image, name, price }) => {
+  const url = image && image.url;
+  return (
+    <article className="product">
+      <img src={url || defaultImage} alt={name || "default name"} />
+      <h4>{name}</h4>
+      <p>${price || 3.99}</p>
+      {/* <h4>single item</h4> */}
+    </article>
+  );
 };
-
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+// Product.defaultProps = {
+//   name: "dafault name",
+//   price: 3.99,
+//   image: defaultImage,
+// };
 export default Product;
