3a7390a727539c4d6b498db606f62d5b1f7beb2d
Charan-ravi <charan2ravi@gmail.com>
2022-03-30 12:45:00 +0530

    saved

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 // import Setup from './tutorial/1-useState/setup/2-useState-basics'
 // import Setup from "./tutorial/10-prop-types/setup";
 // import Final from "./tutorial/11-react-router/final";
-import Setup from "./tutorial/11-react-router/setup";
+import Setup from "./tutorial/12-memo-useMemo-useCallback/setup";
 function App() {
   return (
     <div className="container">
