d6fbda9b865dbf77bf67fd3b52ee94bdf37ab89a
Charan-ravi <charan2ravi@gmail.com>
2022-03-30 11:02:18 +0530

    save progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import Setup from './tutorial/1-useState/setup/1-error-example'
 // import Setup from './tutorial/1-useState/setup/2-useState-basics'
-import Setup from "./tutorial/1-useState/setup/5-useState-counter";
+import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
 function App() {
   return (
     <div className="container">
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("call use effect");
+    if (value > 1) {
+      document.title = `New Messages(${value})`;
+    }
+  }, [value]);
+  const increase = () => {
+    return setValue(value + 1);
+  };
+  useEffect(() => {
+    console.log("hello world");
+  },[]);
+  console.log("render component");
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={increase}>
+        click me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
